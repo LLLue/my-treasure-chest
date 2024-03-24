f815c4721b8b6edbf1ace69d2a521e734de90bf6
LLLue <940406476@qq.com>
2024-03-25 00:21:24 +0800

    链表创建练习

--- c_test.c
+++ c_test.c
@@ -1,14 +1,66 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int a = 1;
-#define A   a
+typedef int ElemType;/* 简单数据类型 */
+
+typedef struct tagNode
+{
+    ElemType  data;
+    struct tagNode *next;
+}LNode, *LinkList;
+
+
+void input(ElemType *ep)
+{
+    scanf("%d",ep);
+
+}
+
+void CreatLinkF(LinkList *L, int n, void(* input)(ElemType*))
+{
+    //头插法创建单链表
+    LinkList s;
+    *L = (LinkList)malloc(sizeof(LNode));
+    (*L)->next = NULL;
+    for(; n>0 ;n--)
+    {
+        s = (LinkList)malloc(sizeof(LNode));
+        input(&s->data);
+        s->next = (*L)->next;
+        (*L)->next = s;
+    }
+
+}
+
+/* 输出链表 */
+void visit(ElemType *ep)
+{
+    printf("%d",*ep);
+}
+
+
+/* 链表遍历 */
+void ListTraverse(LinkList L, void( * vist)(ElemType *))
+{
+    //遍历L中的每个元素且调用Visit函数访问它
+    LinkList p = L->next;
+    while( p != NULL)
+    {
+        visit(&(p->data));
+        p = p->next;
+    }
+}
 
 int main()
 {
-    printf("a = %d!",a);
 
-    a = 2;
-    printf("A = %d, a = %d!",A,a);
+    LinkList L;
+
+    CreatLinkF(&L,5,input);
+
+    printf("print List Begin!\n");1
+
+    ListTraverse(L,visit);
 
     return 0;
 }
