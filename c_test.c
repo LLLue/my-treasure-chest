0edf2369c74707df6549da983ecc1e5a9766c2eb
LLLue <940406476@qq.com>
2024-03-29 20:47:58 +0800

    modified:   c_test.c 	new file:   game.c

--- c_test.c
+++ c_test.c
@@ -1,50 +1,74 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef int ElemType;/* 简单数据类型 */
+typedef int ElemType; /* 简单数据类型 */
 
 typedef struct tagNode
 {
-    ElemType  data;
+    ElemType data;
     struct tagNode *next;
-}LNode, *LinkList;
-
+} LNode, *LinkList;
 
 void input(ElemType *ep)
 {
-    scanf("%d",ep);
-
+    scanf("%d", ep);
 }
 
-void CreatLinkF(LinkList *L, int n, void(* input)(ElemType*))
+void CreatLinkF(LinkList *L, int n, void (*input)(ElemType *))
 {
-    //头插法创建单链表
+    // 头插法创建单链表
     LinkList s;
     *L = (LinkList)malloc(sizeof(LNode));
     (*L)->next = NULL;
-    for(; n>0 ;n--)
+    for (; n > 0; n--)
     {
         s = (LinkList)malloc(sizeof(LNode));
         input(&s->data);
         s->next = (*L)->next;
         (*L)->next = s;
     }
+}
 
+void CreatLinkFuncR(LinkList *L, int n, void (*input)(ElemType *))
+{
+    // 尾插法创建单链表
+    *L = (LinkList)malloc(sizeof(LNode));
+    LinkList s, r;
+    r = L;
+    for (; n > 0; n--)
+    {
+        s = (LinkList)malloc(sizeof(LNode));
+        input(&s->data);
+        r->next = s;
+        r = s;
+    }
+    r->next = NULL;
 }
 
 /* 输出链表 */
 void visit(ElemType *ep)
 {
-    printf("%d",*ep);
+    printf("%d", *ep);
 }
 
-
 /* 链表遍历 */
-void ListTraverse(LinkList L, void( * vist)(ElemType *))
+void ListTraverse(LinkList L, void (*vist)(ElemType *))
 {
-    //遍历L中的每个元素且调用Visit函数访问它
+    // 遍历L中的每个元素且调用Visit函数访问它
     LinkList p = L->next;
-    while( p != NULL)
+    while (p != NULL)
+    {
+        visit(&(p->data));
+        p = p->next;
+    }
+}
+
+/* 尾插法链表遍历 */
+void ListTraverseR(LinkList L, void (*vist)(ElemType *))
+{
+    // 遍历L中的每个元素且调用Visit函数访问它
+    LinkList p = L;
+    while (p != NULL)
     {
         visit(&(p->data));
         p = p->next;
@@ -54,13 +78,19 @@ void ListTraverse(LinkList L, void( * vist)(ElemType *))
 int main()
 {
 
-    LinkList L;
+    LinkList L, S;
 
-    CreatLinkF(&L,5,input);
+    S = L;
 
-    printf("print List Begin!\n");1
+    CreatLinkFuncR(&L, 5, input);
 
-    ListTraverse(L,visit);
+    printf("print List Begin!\n");
+
+    ListTraverseR(S, visit);
+
+    /* 熟悉一下vscode阅读代码快捷键 */
+
+    ListTraverseR(S, visit);
 
     return 0;
 }
